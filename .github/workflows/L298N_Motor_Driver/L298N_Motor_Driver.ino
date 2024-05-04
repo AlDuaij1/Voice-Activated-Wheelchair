3aeb6c166e06e23e02bcadd4de51aada4291e443
AlDuaij1 <a.a.duaij1@gmail.com>
2024-05-04 06:33:50 +0300

    Add files via upload

---
+++ .github/workflows/L298N_Motor_Driver/L298N_Motor_Driver.ino
@@ -0,0 +1,100 @@
+
+#define Lpwm_pin  5     //pin of controlling speed---- ENA of motor driver board
+#define Rpwm_pin  6    //pin of controlling speed---- ENB of motor driver board
+int pinLB=2;             //pin of controlling turning---- IN1 of motor driver board
+int pinLF=4;             //pin of controlling turning---- IN2 of motor driver board
+int pinRB=7;            //pin of controlling turning---- IN3 of motor driver board
+int pinRF=8;            //pin of controlling turning---- IN4 of motor driver board
+// This program shown how to control arduino from PC Via Bluetooth
+// Connect ...
+// arduino>>bluetooth
+// D11   >>>  Rx
+// D10   >>>  Tx
+//Written By Mohannad Rawashdeh
+//for http://www.genotronex.com/
+
+// you will need arduino 1.0.1 or higher to run this sketch
+
+#include <SoftwareSerial.h>// import the serial library
+
+SoftwareSerial mySerial(11, 10); // RX, TX
+//int ledpin=13; // led on D13 will show blink on / off
+int cmd; // the data given from Computer
+
+void setup()
+{
+  pinMode(pinLB,OUTPUT); // /pin 2
+  pinMode(pinLF,OUTPUT); // pin 4
+  pinMode(pinRB,OUTPUT); // pin 7
+  pinMode(pinRF,OUTPUT);  // pin 8
+  pinMode(Lpwm_pin,OUTPUT);  // pin 5 (PWM) 
+  pinMode(Rpwm_pin,OUTPUT);  // pin 6(PWM)   
+  // put your setup code here, to run once:
+  mySerial.begin(9600);
+  Serial.begin(9600);
+  //pinMode(ledpin,OUTPUT);
+}
+
+
+void loop()
+{go_forward(100);
+ delay(2000);
+ go_backward(100);
+ delay(2000);
+ rotate_left(150);
+ delay(2000);
+ rotate_right(150);
+ delay(2000);
+ stopp();
+ delay(2000);
+ }
+
+
+void go_forward(unsigned char speed_val)    // speed_val：0~255
+    {digitalWrite(pinRB,HIGH); 
+     digitalWrite(pinRF,LOW);
+     digitalWrite(pinLB,HIGH);
+     digitalWrite(pinLF,LOW);
+     analogWrite(Lpwm_pin,speed_val);
+     analogWrite(Rpwm_pin,speed_val);
+     
+      
+    }
+
+void go_backward(unsigned char speed_val)    // speed_val：0~255
+    {
+     digitalWrite(pinRB,LOW);  
+     digitalWrite(pinRF,HIGH);
+     digitalWrite(pinLB,LOW);  
+     digitalWrite(pinLF,HIGH);
+     analogWrite(Lpwm_pin,speed_val);
+     analogWrite(Rpwm_pin,speed_val);
+    }
+    
+void rotate_left(unsigned char speed_val)        // speed_val：0~255
+    {digitalWrite(pinRB,HIGH);
+     digitalWrite(pinRF,LOW );  
+     digitalWrite(pinLB,LOW); 
+     digitalWrite(pinLF,HIGH);
+     analogWrite(Lpwm_pin,speed_val);
+     analogWrite(Rpwm_pin,speed_val);
+      
+     
+    }
+void rotate_right(unsigned char speed_val)    // speed_val：0~255
+    {
+      digitalWrite(pinRB,LOW);  
+     digitalWrite(pinRF,HIGH);
+     digitalWrite(pinLB,HIGH);
+     digitalWrite(pinLF,LOW);  
+     analogWrite(Lpwm_pin,speed_val);
+     analogWrite(Rpwm_pin,speed_val);
+     
+    }    
+void stopp()        //stop
+    {
+     digitalWrite(pinRB,HIGH);
+     digitalWrite(pinRF,HIGH);
+     digitalWrite(pinLB,HIGH);
+     digitalWrite(pinLF,HIGH);
+    }
